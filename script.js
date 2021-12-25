0562cb5bab5be80d918bf173a41683277f87214a
vitorhugo1207 <vitorhugoalvesferreira@gmail.com>
2021-12-25 16:28:56 -0300

    Content

---
+++ script.js
@@ -0,0 +1,39 @@
+class Portifolio{
+  constructor(){
+    this.bio = document.querySelector("#bio")
+    this.pic = document.querySelector("#pic")
+    this.about = document.querySelector("#about-content")
+  }
+
+  async setPic() {
+    try{
+      let response = await fetch("https://api.github.com/users/vitorhugo1207");
+      let responseJson = await response.json();
+      this.pic.src = responseJson.avatar_url;
+    }
+    catch{
+      this.pic.src = "error_img.jpg"
+      this.pic.style = "border-radius: 0%;"
+    }
+  }
+  async setBio(){
+    try{
+      let response = await fetch("contents/bio_home.txt");
+      let responseText = await response.text();
+      this.bio.innerHTML = responseText;
+    }
+    catch{
+      this.bio.innerHTML = "Error! Tente ativar o JavaScript do seu navedador.";
+    }
+  }
+  async setAbout(){
+    try{
+      let response = await fetch("contents/about_me.txt");
+      let responseText = await response.text();
+      this.bio.innerHTML = responseText;
+    }
+    catch{
+
+    }
+  }
+}
