8cfa9ab5d6b749265c71eab342f73191ad411bab
vitorhugo1207 <vitorhugoalvesferreira@gmail.com>
2022-01-05 11:59:09 -0300

    up

--- script.js
+++ script.js
@@ -5,6 +5,22 @@ class Portifolio{
     this.about = document.querySelector("#about-content");
   };
 
+  async init(){
+    let title = document.title;
+
+    if (title == "Projetos - Vitor Hugo's Portifolio" || title == "Projects - Vitor Hugo's Portfolio"){
+      this.setProjects()
+    };
+    if (title == "Home - Vitor Hugo's Portifolio" || title == "Home - Vitor Hugo's Portfolio"){
+      alert("Esse site ainda está sendo construido, então pode ter alguns bugs ou informações incompletas.");
+      this.setBio();
+      this.setPic()
+    };
+    if (title == "Sobre - Vitor Hugo's Portifolio" || title == "About - Vitor Hugo's Portfolio"){
+      this.setAbout()
+    };
+  };
+
   async setPic() {
     try{
       let response = await fetch("https://api.github.com/users/vitorhugo1207");
@@ -12,8 +28,8 @@ class Portifolio{
       this.pic.src = responseJson.avatar_url;
     }
     catch{
-      this.pic.src = "error_img.jpg"
-      this.pic.style = "border-radius: 0%;"
+      this.pic.src = "error_img.jpg";
+      this.pic.style = "border-radius: 0%;";
     }
   };
 
@@ -24,7 +40,7 @@ class Portifolio{
       this.bio.innerHTML = responseText;
     }
     catch{
-      this.bio.innerHTML = "Error! Tente ativar o JavaScript do seu navedador.";
+      this.bio.innerHTML = "Error! Tente ativar o JavaScript do seu navedador."
     }
   };
 
@@ -39,18 +55,20 @@ class Portifolio{
     }
   };
 
-  async setProjects(){
-    let response = await fetch("contents/projects.json");
-    let responseJson = await response.json();
+  setProjects() {
+    document.addEventListener("DOMContentLoaded", function() {
+      response_promise.then(response => response.json().then(responseJson => {
+        var p = document.createElement('p');
 
-    document.addEventListener("DOMContentLoaded", function () {
-      var p = document.createElement('p');
+        p.innerHTML = responseJson.one.bio;
+        p.id = "bio-project";
+        // p.style.cssText = 'border-left: 8px solid aqua;';
 
-      p.innerHTML = responseJson.one.bio;
-      p.id = "bio-project";
-      // p.style.cssText = 'border-left: 8px solid aqua;';
-
-      document.querySelector(".projeto-contents").appendChild(p);
+        document.querySelector(".projeto-contents").appendChild(p);
+      }))
     }, false)
   }
 }
+
+const portifolio = new Portifolio();
+portifolio.init();
